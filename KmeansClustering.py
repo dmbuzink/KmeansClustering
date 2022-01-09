0465597ffd9fd6c09f689373ab7088b9d7388564
Damian M. Buzink <dbuzink@gmail.com>
2022-01-09 13:00:00 +0100

    Worked out rough idea

---
+++ KmeansClustering.py
@@ -0,0 +1,32 @@
+def merge_coresets(coreset_a: [], coreset_b: []):
+    return coreset_a + coreset_b
+
+
+
+def compute_coreset(points):
+    """
+    Takes a set of points and computes the coreset for these points.
+
+    :param points: an array of X [Idk, yet, maybe a class like vertex? Do we want d-dimensions?]
+    :return: an array of X [see above], which are representative points.
+    """
+    return [] # set of representatives
+
+
+# Main function
+def main() -> None:
+    # Datasets
+
+    # Split all points over x sets
+    # Coresets = generate coresets across m machines, with the points split across the machines roughly equally
+    # while amount of coresets > 1:
+    #   Merge coresets into pairs. (1, 2, 3, 4, 5, 6 -> Q1: (1, 2), Q2: (3, 4), Q3: (5, 6))
+    #   coresets = compute coresets across machines
+
+    return # statement to get my debugger of my back :)
+
+
+if __name__ == '__main__':
+    # Initial call to main function
+    main()
+
