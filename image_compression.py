4e2475c8350164722dd09dfe2bdd1c325ed06d05
Damian M. Buzink <dbuzink@gmail.com>
2022-01-12 15:52:45 +0100

    Some code added to start the image compression

---
+++ image_compression.py
@@ -0,0 +1,38 @@
+from skimage import io
+import numpy as np
+
+# CONFIG
+image_file_name = "test_image.jpg"
+
+
+#Read the image
+image = io.imread('images\\' + image_file_name)
+io.imshow(image)
+# io.show()
+
+#Dimension of the original image
+rows = image.shape[0]
+cols = image.shape[1]
+
+#Flatten the image
+image = image.reshape(rows*cols, 3)
+# array of [R, G, B] arrays
+
+# #Implement k-means clustering to form k clusters
+# kmeans = KMeans(n_clusters=4)
+# kmeans.fit(image)
+
+# #Replace each pixel value with its nearby centroid
+# compressed_image = kmeans.cluster_centers_[kmeans.labels_]
+
+kmeans_clustering = []
+
+compressed_image = np.clip(kmeans_clustering.astype('uint8'), 0, 255)
+
+#Reshape the image to original dimension
+compressed_image = compressed_image.reshape(rows, cols, 3)
+
+#Save and display output image
+io.imsave('images\\compr_' + image_file_name, compressed_image)
+io.imshow(compressed_image)
+io.show()
