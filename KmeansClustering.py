b059058f6c5149089615bd71eb3f11b348389569
Tobias van Elk <vanelktobias@gmail.com>
2022-01-13 20:21:26 +0100

    Implement coreset construction locally

--- KmeansClustering.py
+++ KmeansClustering.py
@@ -5,6 +5,7 @@ import numpy as np
 from matplotlib import pyplot as plt
 from sklearn.datasets import make_circles, make_moons, make_blobs 
 from sklearn.cluster import kmeans_plusplus
+from sklearn.cluster import KMeans
 
 from pyspark import SparkConf, SparkContext, RDD
 
@@ -13,37 +14,75 @@ from Vertex import Vertex
 spark: SparkContext = None
 
 def create_blobs() -> Tuple[np.ndarray, np.ndarray]:
-    n_samples = 100
+    n_samples = 100000
     n_clusters = 3
 
     return make_blobs(n_samples=n_samples, random_state=8, centers=n_clusters)
 
 def perform_clustering(P: np.ndarray, k: int, epsilon: float = 0.1, num_partitions: int = 10) -> np.ndarray:
 
-    global spark
+    pass
+    # global spark
     
-    # Create vertices for points
-    vertices = [(i, Vertex(i, P[i][0], P[i][1], 1)) for i in range(len(P))]
-    # Parallelize
-    data = spark.parallelize(vertices)
-    # Divide among machines
+    # # Create vertices for points
+    # vertices = [(i, Vertex(i, P[i][0], P[i][1], 1)) for i in range(len(P))]
+    # # Parallelize
+    # data = spark.parallelize(vertices)
+    # # Divide among machines
+    # B = len(vertices) / num_partitions
+    # data = data.map(lambda v: (math.floor(v[0] / B), v[1]))
+    # # Calculate coreset
+    # data = data.groupByKey().map(bind_coreset_construction(k, epsilon))
+    # # Flatten into list of vertices
+    # data_collect = data.reduce(lambda a, b: (min(a[0], b[0]), a[1] + b[1]))[1]
+
+    # # Merge and recalculate coreset...
+    # # ...
+
+    # # (Re)construct numpy matrix from vertices
+    # return np.array([[v.x, v.y] for v in data_collect])
+
+
+def kmeans_local(P: np.ndarray, k: int, epsilon: float = 0.1, num_partitions: int = 10) -> Tuple[np.ndarray, np.ndarray]:
+
+    # Create list of vertices
+    vertices = [Vertex(i, P[i][0], P[i][1], 1) for i in range(len(P))]
+    # Define bucket size
     B = len(vertices) / num_partitions
-    data = data.map(lambda v: (math.floor(v[0] / B), v[1]))
-    # Calculate coreset
-    data = data.groupByKey().map(bind_coreset_construction(k, epsilon))
-    # Flatten into list of vertices
-    data_collect = data.reduce(lambda a, b: (min(a[0], b[0]), a[1] + b[1]))[1]
+    # Divide vertices among buckets
+    vertices_dict: Dict[int, List[Vertex]] = {i: [] for i in range(num_partitions)}
+    for v in vertices:
+        key = math.floor(v.index / B)
+        vertices_dict[key].append(v)
+    data = list(vertices_dict.items())
 
-    # Merge and recalculate coreset...
-    # ...
+    # Compute coreset
+    data = list(map(bind_coreset_construction(k, epsilon), data))
 
-    # (Re)construct numpy matrix from vertices
-    return np.array([[v.x, v.y] for v in data_collect])
+    # Combine and recompute while we have more than one group
+    while len(data) > 1:
+        # Combine pairs
+        i = 0
+        while i < len(data) - 1:
+            data[i] = (data[i][0], data[i][1] + data[i + 1][1])
+            data.pop(i + 1)
+            i += 1
+        # Recompute coreset
+        if len(data) == 1:
+            break
+        data = list(map(bind_coreset_construction(k, epsilon), data))
 
-def paoi(a, b):
-    print(f"a: {a}")
-    print(f"b: {a}")
-    return (a[0], a[0] + b[0])
+    result = data[0][1]
+    print(f"Result points: {len(result)}")
+
+    # Perform K-means clustering on result
+    # Reconstruct numpy matrix
+    points = np.array([[v.x, v.y] for v in result])
+    weights = np.array([v.weight for v in result])
+
+    kmeans = KMeans(n_clusters=k, random_state=0).fit(points, sample_weight=weights)
+
+    return np.array([[v.x, v.y] for v in result]), kmeans.labels_
 
 def bind_coreset_construction(k: int, epsilon: float):
     def coreset_construction(input: Tuple[int, List[Vertex]]) -> Tuple[int, List[Vertex]]:
@@ -71,7 +110,7 @@ def bind_coreset_construction(k: int, epsilon: float):
 
             def point_in_ball(p, c) -> bool:
                 dist = abs(np.linalg.norm(p - c))
-                return dist <= r_outer and dist > r_inner
+                return dist <= r_outer and (dist > r_inner or r_inner == 0.0)
             
             def point2cell(p, c) -> Tuple[int, int]:
                 if not point_in_ball(p, c):
@@ -92,7 +131,7 @@ def bind_coreset_construction(k: int, epsilon: float):
             new_is_representative: List[bool] = []
             # For each ball, keep a dictionary that stores a list per cell containing
             # the weights of the points in that cell
-            weight_of_cell: List[Dict[Tuple[int, int], float]] = [{}] * len(centers)
+            weight_of_cell: Dict[int, Dict[Tuple[int, int], float]] = {i: {} for i in range(len(centers))}
 
             # Iterate over all points and store them in the new_points list
             # or in the cell dictionary
@@ -140,8 +179,10 @@ def bind_coreset_construction(k: int, epsilon: float):
                     new_points.append(cell_c)
                     new_weights.append(weight)
                     new_is_representative.append(True)
+            
+            np_points = np.array(new_points)
 
-            return np.array(new_points), np.array(new_weights), new_is_representative
+            return np_points, np.array(new_weights), new_is_representative
         
         # Calculate the average distance r
         r = 0.0
@@ -152,23 +193,24 @@ def bind_coreset_construction(k: int, epsilon: float):
         r_outer = r
         r_inner = 0.0
 
-        # print(f"Count: {is_representative.count(False)}")
         # Replace points inside balls with a weighted representative point
         points, weights, is_representative = replace_points(points, weights, is_representative, c_points, r_outer, r_inner)
-        # print(f"Count: {is_representative.count(False)}")
-        # Repeat this process until all points are replaced by a rep
-        # j = 1
-        # while False in is_representative:
-        #     r_inner = r_outer
-        #     r_outer = 2**j * r
-        #     points, weights, is_representative = replace_points(points, weights, is_representative, c_points, r_outer, r_inner)
-        #     # print(f"Count: {is_representative.count(False)}")
+
+        j = 1
+        while False in is_representative:
+            r_inner = r_outer
+            r_outer = 2**j * r
+            points, weights, is_representative = replace_points(points, weights, is_representative, c_points, r_outer, r_inner)
+            j += 1
 
         # Return coreset
         vertices: List[Vertex] = []
         for i in range(len(points)):
             p = points[i]
             vertices.append(Vertex(i, p[0], p[1], weights[i]))
+
+        
+        print(f"Subset {input[0]} points: {len(vertices)}")
 
         return (input[0], vertices)
 
@@ -179,20 +221,19 @@ def main() -> None:
 
     global spark
 
-    sparkConf = SparkConf().setAppName('AffinityClustering')
-    spark = SparkContext(conf=sparkConf)
+    # sparkConf = SparkConf().setAppName('AffinityClustering')
+    # spark = SparkContext(conf=sparkConf)
     
     data_X, data_y = create_blobs()
-    clustered = perform_clustering(data_X, 3)
 
-    spark.stop()
+    c_points, c_indices = kmeans_local(data_X, 3)
 
-    # colors = ['#dc79c6' if i in c_indices else '#282a36'  for i in range(len(data_X))]
+    # spark.stop()
 
     fig, ax_data = plt.subplots(nrows=1, ncols=1)
     fig.tight_layout()
     ax_data.set_title("Dataset")
-    ax_data.scatter(clustered[:, 0], clustered[:, 1], marker="o", s=25)
+    ax_data.scatter(c_points[:, 0], c_points[:, 1], marker="o", c=c_indices, s=25)
 
     plt.show()
 
